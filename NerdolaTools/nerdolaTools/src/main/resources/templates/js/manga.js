28f66e436e9f2a0874a690981d1c00e974409c9c
tsemh <tiago_barbosa-silva@hotmail.com>
2023-10-14 17:57:06 -0300

    Update header by client

--- NerdolaTools/nerdolaTools/src/main/resources/templates/js/manga.js
+++ NerdolaTools/nerdolaTools/src/main/resources/templates/js/manga.js
@@ -1,37 +1,62 @@
+console.log(typeof jsPDF);
+
+
 $(document).ready(function() {
     $('#mangaForm').submit(function(e) {
         e.preventDefault();
-
         var url = $('#urlInput').val();
-        var nameZip = $('#nameZipInput').val();
+        var endpoint = $('#endpointSelect').val();
 
         var requestData = {
-            "url": url,
-            "nameZip": nameZip
+            "url": url
         };
-
         $.ajax({
             type: 'POST',
-            url: 'http://localhost:8080/download-manga/mangareader',
+            url: 'http://localhost:8080/manga-download/' + endpoint,
             contentType: 'application/json',
             data: JSON.stringify(requestData),
-            headers: {
-                Accept: 'application/zip'
-            },
             success: function(data) {
-                var blob = new Blob([data], { type: 'application/zip' });
+                console.log("Dados recebidos do servidor:", data);
 
-                var downloadLink = document.createElement('a');
-                downloadLink.href = window.URL.createObjectURL(blob);
-                downloadLink.download = nameZip + '.zip';
-
-                document.body.appendChild(downloadLink);
-                downloadLink.click();
-                document.body.removeChild(downloadLink);
-                console.log(data)
+                if (Array.isArray(data) && data.length > 0) {
+                    createPDFFromImages(data);
+                } else {
+                    console.error('Resposta inválida do servidor.');
+                }
             },
             error: function(error) {
+                console.error(error);
             }
         });
     });
 });
+function createPDFFromImages(imageUrls) {
+    var doc = new jsPDF();
+    function processImage(index) {
+        if (index < imageUrls.length) {
+            var imageUrl = imageUrls[index];
+            loadImage(imageUrl, function(imageData) {
+                doc.addImage(imageData, 'JPEG', 10, 10, 190, 250); // Ajuste os valores conforme necessário
+                doc.addPage();
+                processImage(index + 1);
+            });
+        } else {
+            doc.save('downloaded_pdf.pdf');
+        }
+    }
+    processImage(0);
+}
+function loadImage(url, callback) {
+    var img = new Image();
+    img.crossOrigin = 'Anonymous'; // Permitir o carregamento de imagens de diferentes domínios
+    img.onload = function() {
+        var canvas = document.createElement('canvas');
+        var ctx = canvas.getContext('2d');
+        canvas.width = img.width;
+        canvas.height = img.height;
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        var imageData = canvas.toDataURL('image/jpeg');
+        callback(imageData);
+    };
+    img.src = url;
+}
